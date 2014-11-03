8f45029a5f5f7d44dceca0abb4b0ab676c7823b9
Alessandro Lepore <lepore.ale@gmail.com>
2014-11-03 22:43:55 +0100

    avoid useless newlines
    Conflicts:
    	lib/generators/spree_scaffold/templates/model.rb

--- lib/generators/spree_scaffold/templates/model.rb
+++ lib/generators/spree_scaffold/templates/model.rb
@@ -1,27 +1,27 @@
 module Spree
   class <%= class_name %> < ActiveRecord::Base
-    <% if slugged? %>
+<% if slugged? -%>
     extend FriendlyId
     friendly_id :slug_candidates, use: [:slugged, :finders]
-    <% end %>
-    <% attributes.each do |attribute| %>
-    <% if attribute.type == :image %>
+<% end -%>
+<% attributes.each do |attribute| -%>
+<% if attribute.type == :image -%>
     has_attached_file :<%= attribute.name %>,
                       styles: { large: '600x600>' },
                       default_style: :large,
                       url: '/spree/<%= plural_name %>/:id/:style/:basename.:extension',
                       path: ':rails_root/public/spree/<%= plural_name %>/:id/:style/:basename.:extension',
                       convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
-    <% elsif attribute.type == :file %>
+<% elsif attribute.type == :file -%>
     has_attached_file :<%= attribute.name %>,
                       url: '/spree/<%= plural_name %>/:id/:basename.:extension',
                       path: ':rails_root/public/spree/<%= plural_name %>/:id/:basename.:extension'
-    <% end %>
-    <% end %>
-    <% if slugged? %>
+<% end -%>
+<% end -%>
+<% if slugged? -%>
     def slug_candidates
       [:<%= attributes.find { |a| a.type == :string }.name %>]
     end
-    <% end %>
+<% end -%>
   end
 end
